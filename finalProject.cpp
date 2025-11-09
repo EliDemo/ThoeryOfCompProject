b1445d4d66ef26c656fc71a22db29d11b115daef
kevknowscode <KM.Kairouz@gmail.com>
2025-11-08 19:34:31 -0500

    update 1

--- finalProject.cpp
+++ finalProject.cpp
@@ -1,6 +1,34 @@
 #include <iostream> 
-#include <stack> 
+#include <stack>  
+#include <vector> 
 
+class DPA{ 
+
+    public: 
+
+    
+
+
+    private:  
+
+    //TODO: finish the input variables for the dpa class 
+    std::string input; 
+
+    std::vector<int>step;  
+    std::vector<std::string>state; 
+    std::vector<std::string>unreadInput;  
+    std::vector<std::string>stack;  
+    std::vector<int>gammaRuleUsed; 
+    std::vector<std::string>r_rule_used; 
+
+
+
+
+    
+
+
+
+}; 
 
 
 int main(){ 
